e2355e5fe689156dc5b8dfc158043c07cdce32c9
jmurkijanian <jmurkijanian@gmail.com>
2019-09-07 14:25:19 -0700

    Program that calculates persons height

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,22 @@
 // This program calculates a person's height in feet (') and inches (").
+#include <iostream>
+ int main()
+ {
+int feet, inches, feetinches;
+
+
+//finds inches of person
+std::cout << "How many inches tall is this person?\n";
+std::cin >> inches;
+
+//calculates feet
+feet = inches / 12;
+
+feetinches = inches % 12;
+
+std::cout << "this person is " <<feet<< "\'" <<feetinches<< "\"";
+
+
+return 0;
+
+}
